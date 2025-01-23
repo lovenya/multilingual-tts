4d8582ac4fa3dc510f3635f18cea41df93a81d16
lovenya <lovenya2002@gmail.com>
2025-01-24 00:39:10 +0530

    espeak backend config file added

---
+++ util/phonemizer_espeak-ng_backend_config.py
@@ -0,0 +1,11 @@
+# global config file for espeak backend for phonemizer
+# add import phonemizer_config at the start of every script that uses phonemizer of course
+
+
+import os
+from phonemizer.backend.espeak.wrapper import EspeakWrapper
+
+# Set up eSpeak-NG globally
+EspeakWrapper.set_library("C:\\Program Files\\eSpeak NG\\libespeak-ng.dll")
+os.environ["PATH"] += r";C:\Program Files\eSpeak NG"
+os.environ["ESPEAK_DATA_PATH"] = r"C:\Program Files\eSpeak NG\espeak-ng-data"
